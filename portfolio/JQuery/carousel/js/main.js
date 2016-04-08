e42fb884841c7e29141b2f495576340c933bec6d
OleksandrSachuk <ovsachuk@!gmail.com>
2016-04-08 15:12:59 +0300

    carousel with the comments

--- portfolio/JQuery/carousel/js/main.js
+++ portfolio/JQuery/carousel/js/main.js
@@ -1,41 +1,47 @@
-jQuery(document).ready(function($) {
-	var slider = $('.slider');
-	slider.append('<div class="nav"></div>');
-	slider.find('li').each(function(index, el) {
-		slider.find('.nav').append("<span></span>");
+$(document).ready(function()
+		{
+			//function for every slider
+			$(".slider").each(function ()
+			{
+				//save current slider
+				var obj = $(this);
+				//add navigation block
+				$(obj).append("<div class='nav'></div>");
+
+				//function for every element of the slider
+				$(obj).find("li").each(function ()
+				{
+					//add span with the number of clicked menu number
+					$(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>");
+					//adding class
+					$(this).addClass("slider"+$(this).index());
+				});
+
+				//add class for current menu namber
+				$(obj).find("span").first().addClass("on");
+			});
+		});
+
+	function sliderJS (obj, sl) // slider function
+	{
+		//find ul
+		var ul = $(sl).find("ul");
+		var bl = $(sl).find("li.slider"+obj);
+		//initiate step width 
+		var step = $(bl).width();
+		//animate changing image
+		$(ul).animate({marginLeft: "-"+step*obj}, 500);
+	}
+
+	$(document).on("click", ".slider .nav span", function() // slider click navigate
+	{
+		var sl = $(this).closest(".slider");
+		//remove class from previous active menu element
+		$(sl).find("span").removeClass("on");
+		//add class for active menu element
+		$(this).addClass("on");
+		var obj = $(this).attr("rel");
+		//launch slider function
+		sliderJS(obj, sl);
+		return false;
 	});
-});
-// $(document).ready(function()
-// 		{
-// 			$(".slider").each(function ()
-// 			{
-// 				var obj = $(this);
-// 				$(obj).append("<div class='nav'></div>");
-
-// 				$(obj).find("li").each(function ()
-// 				{
-// 					$(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>");
-// 					$(this).addClass("slider"+$(this).index());
-// 				});
-
-// 				$(obj).find("span").first().addClass("on");
-// 			});
-// 		});
-
-// 	function sliderJS (obj, sl) // slider function
-// 	{
-// 		var ul = $(sl).find("ul");
-// 		var bl = $(sl).find("li.slider"+obj);
-// 		var step = $(bl).width();
-// 		$(ul).animate({marginLeft: "-"+step*obj}, 500);
-// 	}
-
-// 	$(document).on("click", ".slider .nav span", function() // slider click navigate
-// 	{
-// 		var sl = $(this).closest(".slider");
-// 		$(sl).find("span").removeClass("on");
-// 		$(this).addClass("on");
-// 		var obj = $(this).attr("rel");
-// 		sliderJS(obj, sl);
-// 		return false;
-// 	});
